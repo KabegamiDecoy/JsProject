94a8526475782e34b7551dd9c29fe38840c864fc
Kabegami <79057942+KabegamiDecoy@users.noreply.github.com>
2021-12-22 17:26:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var title = 'project1';
+var screens = "Прстые, Сложные, Интерактивные";
+var screenPrice = 135;
+var rollback = 13;
+var fullPrice = 135345;
+var adaptive = true;
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов: "+ screenPrice  + "Стоимость разработки сайта: "+ fullPrice);
+console.log(screens.toLocaleLowerCase().split());
+console.log(Math.trunc(fullPrice * (rollback/100)));
