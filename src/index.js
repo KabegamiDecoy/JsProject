09bb9f00c6a5ca317565481d2da94ef58f89db54
Kabegami <79057942+KabegamiDecoy@users.noreply.github.com>
2022-02-06 21:00:58 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
+import tabs from './modules/tabs'
+import slider from './modules/slider'
+import calc from './modules/calc'
+
+timer('27 january 2022 14:00:00')
+menu()
+modal()
+tabs()
+slider()
+calc(100)
